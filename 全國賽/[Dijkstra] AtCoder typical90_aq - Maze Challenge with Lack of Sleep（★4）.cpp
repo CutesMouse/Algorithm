e01fdab949234256f6129852c4711f2584071bdb
CutesMouse <57491074+CutesMouse@users.noreply.github.com>
2021-12-11 12:29:26 +0800

    Update [Dijkstra] AtCoder typical90_aq - Maze Challenge with Lack of Sleep（★4）.cpp

--- 全國賽/[Dijkstra] AtCoder typical90_aq - Maze Challenge with Lack of Sleep（★4）.cpp
+++ 全國賽/[Dijkstra] AtCoder typical90_aq - Maze Challenge with Lack of Sleep（★4）.cpp
@@ -1,52 +1,96 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 #include <cstring>
 using namespace std;
-typedef pair<int,int> pp;
-typedef priority_queue<pp,vector<pp>,greater<pp> > ppq;
-
-vector<pp> edges[100005];
-int dis_begin[100005],dis_end[100005];
-
-void dij(int* dis, int start, int to)
+typedef pair<int,int> loc;
+int d_x[8000000];
+int d_y[8000000];
+int d_dir[8000000];
+int ptr = 0;
+int dx[4] = {0,-1,0,1};
+int dy[4] = {1,0,-1,0};
+int dis[1000][1000][4];
+void flat(loc &l)
 {
-    ppq q;
-    q.emplace(0,start);
-    dis[start] = 0;
-    while (q.size())
+    l.first --;
+    l.second --;
+}
+int main()
+{
+    memset(dis,-1,sizeof(dis));
+    // 輸入測資
+    int H,W;
+    cin >>H >> W;
+    loc st;
+    cin >> st.first >>st.second;
+    loc ed;
+    cin >>ed.first >> ed.second;
+    flat(st);
+    flat(ed);
+    string maze[1000];
+    for (int i = 0 ; i < H; i++)
     {
-        pp pr = q.top();
-        q.pop();
-        if (pr.first != dis[pr.second]) continue;
-        for (auto edge : edges[pr.second])
+        cin >> maze[i];
+    }
+    // Dijkstra
+    priority_queue<loc,vector<loc>,greater<loc> > pq;
+    d_x[ptr] = st.first;
+    d_y[ptr] = st.second;
+    d_dir[ptr] = 5;
+    pq.emplace(0,ptr);
+    ptr++;
+    while (pq.size())
+    {
+        auto now = pq.top();
+        pq.pop();
+        int x = d_x[now.second];
+        int y = d_y[now.second];
+        int dir = d_dir[now.second];
+        // dir = 5，表示起始，沒有資料
+        // dis[x][y][dir] 不等於現在步數，表示此路徑已被之前的運算取代，不用重複運算
+        if (dir != 5 && dis[x][y][dir] != now.first) continue;
+
+        for (int r = 0; r < 4; r++)
         {
-            if (dis[edge.second] == -1 || dis[edge.second] > pr.first + edge.first)
+            int nx = x + dx[r];
+            int ny = y + dy[r];
+            // 超出地圖範圍
+            if (nx >= H || ny >= W || nx < 0 || ny < 0) continue;
+            // 該格有屏障，無法前行
+            if (maze[nx][ny] == '#') continue;
+            // dir = 5，起點操作
+            if (dir == 5)
             {
-                dis[edge.second] = pr.first + edge.first;
-                q.emplace(dis[edge.second],edge.second);
+                d_x[ptr] = nx;
+                d_y[ptr] = ny;
+                d_dir[ptr] = r;
+                dis[nx][ny][r] = 1;
+                pq.emplace(1,ptr);
+                ptr++;
+                continue;
+            }
+            // 當前步數
+            int ndis = dis[x][y][dir] + (dir != r);
+            // dis = -1 表示沒有被走過
+            // dis > ndis 表示有更好的方式
+            if (dis[nx][ny][r] == -1 || dis[nx][ny][r] > ndis)
+            {
+                d_x[ptr] = nx;
+                d_y[ptr] = ny;
+                d_dir[ptr] = r;
+                dis[nx][ny][r] = ndis;
+                pq.emplace(ndis,ptr);
+                ptr++;
             }
         }
     }
-}
-
-int main()
-{
-    int N,M;
-    cin >> N >> M;
-    int from,to,weight;
-    for (int i = 0; i <M; i++)
-    {
-        cin >> from >> to >> weight;
-        edges[from].emplace_back(weight,to);
-        edges[to].emplace_back(weight,from);
+    
+    int moves = -1;
+    for (int i = 0; i < 4; i++) {
+        int m = dis[ed.first][ed.second][i];
+        if (m == -1) continue;
+        if (moves == -1) moves = m;
+        moves = min(moves,m);
     }
-    memset(dis_begin,-1,sizeof(dis_begin));
-    memset(dis_end,-1,sizeof(dis_end));
-    dij(dis_begin,1,N);
-    dij(dis_end,N,1);
-    for (int i = 1; i <= N; i++) {
-        cout << dis_begin[i] + dis_end[i] << endl;
-    }
-
+    cout << moves - 1 << endl;
 }
