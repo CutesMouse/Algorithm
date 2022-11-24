663330c43b1772d9b1af4f25be3f529cacb328c4
CutesMouse <57491074+CutesMouse@users.noreply.github.com>
2022-11-24 22:02:18 +0800

    Create Lab 1.3.cpp

---
+++ NCU_LAB_111/Lab 1.3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Please enter the two-positive integers: \n";
+    long long int a,b;
+    cin >> a >> b;
+
+    cout << (a-b) << endl;
+}
