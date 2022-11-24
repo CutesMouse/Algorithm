7789e852685031753a04f6de87630afb7e394d70
CutesMouse <57491074+CutesMouse@users.noreply.github.com>
2022-11-24 22:02:03 +0800

    Create Lab 1.2.cpp

---
+++ NCU_LAB_111/Lab 1.2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "My name is xxx.\n";
+    cout << "My student ID is xxxxxxxxx.\n";
+    cout << "My hometown is xxxxxx.\n\n";
+    cout << "\tI love C++!\n";
+}
