7509b34c937e50a0613d81d45317c9d253b634c0
CutesMouse <57491074+CutesMouse@users.noreply.github.com>
2023-07-30 14:07:52 +0800

    Create cold.cpp

---
+++ Kattis/cold.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    int s = 0;
+    while (a--) {
+        int b;
+        cin >> b;
+        if (b < 0) s ++;
+  }
+    cout << s;
+}
